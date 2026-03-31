e4ace968848601afe7a6dea6b15fec6733b59043
Tasmiya011 <tasmiyajalegar@gmail.com>
2026-03-31 20:16:14 +0530

    Time: 0 ms (100%), Space: 42.8 MB (30.76%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -1,41 +1,35 @@
-import java.util.Stack;
-
 class MyQueue {
-    Stack<Integer> s1; // input stack
-    Stack<Integer> s2; // output stack
+    Stack<Integer> s1;
+    Stack<Integer> s2;
 
     public MyQueue() {
         s1 = new Stack<>();
-        s2 = new Stack<>();
+        s2 = new Stack<>(); 
     }
-
-    // Always push to s1
+    
     public void push(int x) {
-        s1.push(x);
+        s1.push(x);      
     }
-
-    // Transfer from s1 to s2 only when s2 is empty
+    
     public int pop() {
-        if (s2.isEmpty()) {
-            while (!s1.isEmpty()) {
+        if(s2.isEmpty()){
+            while(!s1.isEmpty()){
                 s2.push(s1.pop());
             }
         }
         return s2.pop();
     }
-
+    
     public int peek() {
-        if (s2.isEmpty()) {
-            while (!s1.isEmpty()) {
+        if(s2.isEmpty()){
+            while(!s1.isEmpty()){
                 s2.push(s1.pop());
+                }
             }
-        }
-        return s2.peek();
-    }
-
-    // Queue is empty only when both stacks are empty
+            return s2.peek();
+            }
     public boolean empty() {
-        return s1.isEmpty() && s2.isEmpty();
+        return s1.isEmpty() && s2.isEmpty();       
     }
 }
 
