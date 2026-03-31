900a6fef2e35a630d2cbfa26a70431fbc0d6bef0
Tasmiya011 <tasmiyajalegar@gmail.com>
2026-03-31 19:51:36 +0530

    Time: 0 ms (100%), Space: 42.6 MB (85.48%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -5,29 +5,26 @@ class MyStack {
     public MyStack() {}
     
     public void push(int x) {
-        while(!q1.isEmpty()) {
+        while(!q1.isEmpty()){
             q2.offer(q1.poll());
         }
         q1.offer(x);
 
         while(!q2.isEmpty()){
             q1.offer(q2.poll());
-        }
+        }    
     }
     
     public int pop() {
         return q1.poll();
-        
     }
     
     public int top() {
-        return q1.peek();
-        
+        return q1.peek();        
     }
     
     public boolean empty() {
         return q1.isEmpty();
-        
     }
 }
 
