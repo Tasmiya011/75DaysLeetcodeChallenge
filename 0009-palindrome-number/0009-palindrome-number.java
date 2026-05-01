624a3dea9258477aef878408599329b307b12956
Tasmiya011 <tasmiyajalegar@gmail.com>
2026-05-01 10:49:43 +0530

    Time: 5 ms (79.64%), Space: 46.1 MB (34.48%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int temp = x , rev = 0;
+        if(x<0){
+            return false;
+        }
+        while(temp > 0){
+            int dig = temp % 10;
+            rev = rev * 10 + dig;
+            temp = temp/10;
+        }
+        return (rev == x);
+    }
+}
