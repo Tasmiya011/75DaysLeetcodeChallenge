472948ed1af8fa760d95236cc0ae7b55692ae217
Tasmiya011 <tasmiyajalegar@gmail.com>
2026-05-09 10:48:01 +0530

    Time: 0 ms (100%), Space: 41.8 MB (83.25%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.java
+++ 0319-bulb-switcher/0319-bulb-switcher.java
@@ -1,8 +1,8 @@
 class Solution {
-    public int bulbSwitch(int n) {
-        int count = 0;
+    public int bulbSwitch(int n) {    
+        int count = 0;    // counts the perfect squares
 
-        int i = 1;
+        int i = 1;    // checks the current no starting from 1 till n 
         while(i * i <= n){
             count++;
             i++;
