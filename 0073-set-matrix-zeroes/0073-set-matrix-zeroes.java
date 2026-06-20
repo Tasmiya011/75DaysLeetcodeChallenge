fda2b829fadd128d1851a552d9776c7374013d8b
Tasmiya011 <tasmiyajalegar@gmail.com>
2026-06-20 02:48:27 -0700

    Time: 1 ms (98.41%), Space: 47.7 MB (25.1%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -2,23 +2,46 @@ class Solution {                               //SDE
     public void setZeroes(int[][] matrix) {
         int rows = matrix.length;
         int cols = matrix[0].length;
-        Set<Integer> rowsSet = new HashSet<>();
-        Set<Integer> colsSet = new HashSet<>();
 
-        for (int i = 0; i < rows; i++){
-            for(int j = 0; j < cols; j++){
+        boolean firstRowZero = false;
+        boolean firstColzero = false;
+
+        for(int j=0; j < cols; j++){
+            if(matrix[0][j] == 0){
+                firstRowZero = true;
+                break;
+            }
+        }
+        for(int i=0; i < rows; i++){
+            if(matrix[i][0] == 0){
+                firstColzero = true;
+                break;
+            }
+        }
+        for(int i=1; i<rows; i++){
+            for(int j=1; j<cols; j++){
                 if(matrix[i][j] == 0){
-                    rowsSet.add(i);
-                    colsSet.add(j);
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
                 }
             }
         }
-        for(int i=0; i<rows; i++){
-            for(int j=0; j<cols; j++){
-                if(rowsSet.contains(i) || colsSet.contains(j)){
+            for(int i=1; i<rows; i++){
+                for(int j=1; j<cols; j++){
+                if(matrix[0][j] == 0 || matrix[i][0] == 0){
                     matrix[i][j] = 0;
                 }
             }
-        }        
+        }
+        if(firstRowZero){
+            for(int j = 0; j<cols; j++){
+                matrix[0][j] = 0;
+            }
+        }
+            if(firstColzero){
+            for(int i=0; i<rows; i++){
+                matrix[i][0] = 0;
+            }
+        }
     }
 }
