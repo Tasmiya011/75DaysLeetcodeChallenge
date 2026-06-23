f2a82712a77d6c34f04eaa383d5e5332bf2054e5
Tasmiya011 <tasmiyajalegar@gmail.com>
2026-06-23 05:49:53 -0700

    Time: 2 ms (69.72%), Space: 56 MB (6.41%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int ME = 0, count = 0;
+
+        for(int num : nums){
+            if(count == 0){
+                ME = num;
+            }
+            if(num == ME ){
+                count++;
+            }else{
+                count--;
+            }
+        }
+        return ME;
+    }
+}
