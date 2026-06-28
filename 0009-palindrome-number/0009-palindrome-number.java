fe35b8a494170d0a66ed5083f6a85ceef0ed2649
Tasmiya011 <tasmiyajalegar@gmail.com>
2026-06-28 01:42:07 -0700

    Time: 5 ms (79.58%), Space: 45.7 MB (86.74%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,14 +1,14 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        int temp = x , rev = 0;
-        if(x<0){
+        int temp = x, rev = 0;
+        if(x < 0){
             return false;
         }
         while(temp > 0){
-            int dig = temp % 10;
+            int dig = temp % 10;    //retrives the last digit and keep it
             rev = rev * 10 + dig;
             temp = temp/10;
         }
-        return (rev == x);
+        return (rev==x);
     }
 }
